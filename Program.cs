5600a54d2be7820c3c7c676a39e8b27c7a4dbed0
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2024-02-14 12:31:00 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace edu.PR.Ejercicio7._0502
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String fechaUsuario;
+                Console.WriteLine("Introduzca una fecha: dd/MM/YYYY");
+            fechaUsuario = Console.ReadLine();
+
+            int numeroUsuario;
+            Console.WriteLine("Introduzca el numero de dias con el que quiera operar");
+            numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            DateTime usuarioFecha = Convert.ToDateTime(fechaUsuario);
+            Console.WriteLine(usuarioFecha.ToString());
+
+            DateTime sumaDias = usuarioFecha.AddDays(numeroUsuario);
+            Console.WriteLine("Suma de los dias: " + sumaDias.ToString());
+            DateTime restaDias = usuarioFecha.AddDays(-numeroUsuario);
+            Console.WriteLine("Resta de los dias: "+ restaDias.ToString());
+        }
+    }
+}
